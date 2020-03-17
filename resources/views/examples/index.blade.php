835199d41e5e0490eeb953fa3a68219fc4af5b8b
Jack Clayton <clayjs0@gmail.com>
2020-03-17 17:34:16 -0600

    Add example

---
+++ resources/views/examples/index.blade.php
@@ -0,0 +1,11 @@
+<?php
+use Jahuty\Jahuty\{Jahuty, Snippet};
+
+Jahuty::setKey("kn2Kj5ijmT2pH6ZKqAQyNexUqKeRM4VG6DDgWN1lIcc");
+?>
+<h1>
+  Welcome to Jahuty!
+</h1>
+<p>
+    <?php echo Snippet::get(1); ?>
+</p>
