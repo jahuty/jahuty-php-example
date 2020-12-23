8d725845c9c9880f95d3776b3f6e43a749c2659f
Jack Clayton <clayjs0@gmail.com>
2020-12-23 11:47:52 -0700

    Change from static to instance syntax

--- resources/views/examples/index.blade.php
+++ resources/views/examples/index.blade.php
@@ -1,11 +1,9 @@
 <?php
-use Jahuty\Jahuty\{Jahuty, Snippet};
-
-Jahuty::setKey("kn2Kj5ijmT2pH6ZKqAQyNexUqKeRM4VG6DDgWN1lIcc");
+    $jahuty = new \Jahuty\Client("kn2Kj5ijmT2pH6ZKqAQyNexUqKeRM4VG6DDgWN1lIcc");
 ?>
 <h1>
   Welcome to Jahuty!
 </h1>
 <p>
-    <?php echo Snippet::render(1); ?>
+    <?php echo $jahuty->snippets->render(1); ?>
 </p>
