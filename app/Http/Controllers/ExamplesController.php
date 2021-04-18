641ae972a73d1f502555b775c054ab22d0d4a478
Jack Clayton <clayjs0@gmail.com>
2021-04-18 07:50:22 -0600

    Add examples controller

--- app/Http/Controllers/ExamplesController.php
+++ app/Http/Controllers/ExamplesController.php
@@ -10,4 +10,34 @@ class ExamplesController extends Controller
     {
         return view('examples/index');
     }
+
+    public function snippet(Request $request)
+    {
+        return view('examples/snippet');
+    }
+
+    public function snippetWithParams(Request $request)
+    {
+        return view('examples/snippet-with-params');
+    }
+
+    public function snippetFromCache(Request $request)
+    {
+        return view('examples/snippet-from-cache');
+    }
+
+    public function snippets(Request $request)
+    {
+        return view('examples/snippets');
+    }
+
+    public function snippetsWithParams(Request $request)
+    {
+        return view('examples/snippets-with-params');
+    }
+
+    public function snippetsFromCache(Request $request)
+    {
+        return view('examples/snippets-from-cache');
+    }
 }
