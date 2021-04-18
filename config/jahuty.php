90d45ea666ffc18ca29b5d87a89d8ab5466f24a1
Jack Clayton <clayjs0@gmail.com>
2021-04-18 07:48:20 -0600

    Add api key to config

---
+++ config/jahuty.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /**
+     * WARNING! Checking API keys into source code is a bad idea. It increases
+     * the risk of unauthorized access and should be avoided.
+     *
+     * To protect your API keys from unauthorized access, you should store them
+     * as environment variables, which are not checked into source control, and
+     * retrieve them like this:
+     *
+     * 'api_key' => env('JAHUTY_KEY'),
+     *
+     * We ignored our own advice here to keep the app easy to use.
+     */
+    'api_key' => 'kn2Kj5ijmT2pH6ZKqAQyNexUqKeRM4VG6DDgWN1lIcc'
+];
