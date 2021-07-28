5e707bf13a06b469e0c39415ab4c628cd87df382
Jack Clayton <clayjs0@gmail.com>
2021-07-28 15:27:26 -0600

    Update configuration option name

--- resources/views/examples/snippet-with-latest.blade.php
+++ resources/views/examples/snippet-with-latest.blade.php
@@ -4,8 +4,6 @@
     <?php
         // By default, Jahuty will render a snippet's _published_ content. To
         // render its _latest_ content, use the `prefer_latest_content` option.
-        echo $jahuty->snippets->render(102, [
-            'prefer_latest_content' => true
-        ]);
+        echo $jahuty->snippets->render(102, ['prefer_latest' => true]);
     ?>
 @stop
